1d721655a7e257ca8a1cffa990118b4e3f6718fc
Will Farley <will@django.nu>
2014-05-22 19:59:24 -0700

    adds import script to mktheme.sh

--- mktheme.sh
+++ mktheme.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 read -p "Theme name  : " name
+echo ""
 mkdir ./less/themes/$name
 cp ./less/themes/template/* ./less/themes/$name/
 
-echo ""
 echo "New theme $name was made"
+echo ""
+echo "echo -n \"lessc $name.css...\"" >> ./css.sh
+echo "lessc -x ./less/themes/$name/router.less > ./css/$name.min.css" >> ./css.sh
+echo "echo \"done\"" >> ./css.sh
+echo "Compile script for theme added to css.sh"
