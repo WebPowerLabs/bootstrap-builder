4862cbe212147bb6a5ec521276872de8a9f43eaa
Will Farley <will@django.nu>
2014-05-23 12:53:35 -0700

    adds html templates

--- mksection.sh
+++ mksection.sh
@@ -3,10 +3,11 @@
 
 read -p "section name  : " name
 echo ""
-touch ./less/sections/$name.less
+touch ./less/sections/$name\.less
+cp ./html/sections/template.html ./html/sections/$name.html
 
 echo "New section, $name, created"
 echo ""
-echo "\n@import \"@{section-path}$name.less\";" >> ./less/routers/section.less
+echo "@import \"@{section-path}$name.less\";" >> ./less/routers/section.less
 echo "Import statement for $name added to ./less/routers/section.less"
 echo ""
---
+++ serve.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Starts simple python server for sections
+
+echo "Starting python server\n"
+echo "visit /html/sections/name.html to view section\n"
+echo "command c to close\n"
+
+python -m SimpleHTTPServer 8000
