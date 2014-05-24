5a944c8a6733a57737cadd8fb7eecc7cda7e0a40
zlorenz <zglorenz@gmail.com>
2014-05-24 15:57:39 -0700

    Creates and styles 'Welcome' section. Creates 'Pamukovich' theme.

--- css.sh
+++ css.sh
@@ -3,3 +3,6 @@ echo -n "lessc bootstrap.css..."
 lessc -x ./less/themes/bootstrap/router.less > ./css/bootstrap.min.css
 echo "done"
 
+echo -n "lessc pamukovich.css..."
+lessc -x ./less/themes/pamukovich/router.less > ./css/pamukovich.min.css
+echo "done"
