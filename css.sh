71d56f136fe7823811f416f43e735a8265bec968
zlorenz <zglorenz@gmail.com>
2014-06-05 18:26:01 -0700

    Creates Green theme. Writes Green theme styles for all elements within existing sections.

--- css.sh
+++ css.sh
@@ -6,3 +6,6 @@ echo "done"
 echo -n "lessc pamukovich.css..."
 lessc -x ./less/themes/pamukovich/router.less > ./css/pamukovich.min.css
 echo "done"
+echo -n "lessc green.css..."
+lessc -x ./less/themes/green/router.less > ./css/green.min.css
+echo "done"
